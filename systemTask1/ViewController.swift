50377624269eaf6f247e078ff931fd767e29b7e9
Mallikarjuna <mallik009.bakka@gmail.com>
2019-12-25 09:49:59 +0530

    Initial Commit

---
+++ systemTask1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  systemTask1
+//
+//  Created by mallikarjuna Bakka on 25/12/19.
+//  Copyright © 2019 mallikarjuna Bakka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ systemTask1Tests/systemTask1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  systemTask1Tests.swift
+//  systemTask1Tests
+//
+//  Created by mallikarjuna Bakka on 25/12/19.
+//  Copyright © 2019 mallikarjuna Bakka. All rights reserved.
+//
+
+import XCTest
+@testable import systemTask1
+
+class systemTask1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
